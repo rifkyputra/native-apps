3e2e506cae2f067d146afe2879103b7e9f7448f9
rifky <rifqempul@gmail.com>
2022-02-13 17:34:00 +0700

    acrylic effect on windows

---
+++ lib/calculator/fluent/calculator_fluent.dart
@@ -0,0 +1,94 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:system_theme/system_theme.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+
+import '../cubit/calculator_cubit.dart';
+import '../shared/parts.dart';
+
+class CalculatorFluent extends StatelessWidget {
+  const CalculatorFluent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CalculatorCubit(),
+      child: FluentApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Calculator',
+        theme: ThemeData(
+          accentColor: SystemTheme.accentInstance.accent.toAccentColor(),
+          scaffoldBackgroundColor: Colors.transparent,
+          brightness: Brightness.dark,
+          // acrylicBackgroundColor: Colors.magenta,
+        ),
+        home: ScaffoldPage(
+          header: Container(
+            height: 38,
+            // color: Colors.blue,
+            alignment: Alignment.topLeft,
+            child: MoveWindow(
+              child: SizedBox(
+                height: 38,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Title'),
+                    Spacer(),
+                    Tooltip(
+                      message: 'minimize',
+                      child: MinimizeWindowButton(),
+                    ),
+                    Tooltip(
+                      message: 'maximize',
+                      child: MaximizeWindowButton(),
+                    ),
+                    Tooltip(
+                      message: 'close',
+                      child: CloseWindowButton(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          content: _Content(),
+        ),
+      ),
+    );
+  }
+}
+
+class _Content extends StatefulWidget {
+  const _Content({Key? key}) : super(key: key);
+
+  @override
+  State<_Content> createState() => _ContentState();
+}
+
+class _ContentState extends State<_Content> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      flutter_acrylic.Window.setEffect(
+        effect: flutter_acrylic.WindowEffect.aero,
+        color: FluentTheme.of(context).micaBackgroundColor.withOpacity(0.2),
+        dark: true,
+      );
+      // setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(child: ResultView()),
+        CalcButton(),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,39 @@
 import 'dart:io';
 
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:jakarta_suite/calculator/adwaita/calculator_adw.dart';
+import 'package:jakarta_suite/calculator/fluent/calculator_fluent.dart';
+import 'package:system_theme/system_theme.dart';
 
 import 'calculator/material/calculator_mt.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && (Platform.isLinux || Platform.isWindows)) {
+    doWhenWindowReady(() {
+      final initialSize = Size(600, 855);
+      appWindow.minSize = initialSize;
+      appWindow.size = initialSize;
+      appWindow.alignment = Alignment.topLeft;
+      appWindow.title = 'Calcs';
+      appWindow.show();
+    });
+  }
+
+  if (Platform.isWindows) {
+    await flutter_acrylic.Window.initialize();
+    await flutter_acrylic.Window.hideWindowControls();
+    // await flutter_acrylic.Window.setWindowAlphaValue(.6);
+    await SystemTheme.accentInstance.load();
+  }
+
+  if (Platform.isLinux) {}
+
   runApp(MyApp());
-
-  doWhenWindowReady(() {
-    final initialSize = Size(600, 855);
-    appWindow.minSize = initialSize;
-    appWindow.size = initialSize;
-    appWindow.alignment = Alignment.center;
-    appWindow.show();
-  });
 }
 
 class MyApp extends StatelessWidget {
@@ -23,6 +41,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     if (Platform.isLinux) {
       return CalculatorAdw();
+    }
+
+    if (Platform.isWindows) {
+      return CalculatorFluent();
     }
 
     return MaterialApp(
